2fd8122f115991c22013aef0eaa141925b59b4ad
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-10-31 12:45:05 +0200

    Remove unnecessary datatype and add figma ccorresponding colors

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_29_202218) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
