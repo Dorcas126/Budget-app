9a49729a2ffaadb4d71dd6e0dfbfac918d9dc479
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-11-01 14:46:32 +0200

    Add documentation comment to the controllers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
+# app/controllers/home_controller.rb
 class HomeController < ApplicationController
-    skip_before_action :authenticate_user!
-    def index
-      if signed_in?
-        redirect_to categories_path
-      else
-        render :index
-      end
+  skip_before_action :authenticate_user!
+  def index
+    if signed_in?
+      redirect_to categories_path
+    else
+      render :index
     end
   end
+end
